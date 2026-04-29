d06c68f8500dc3d09bc2a132aa58f93e35726e6d
Oskar <oskarkaasik@gmail.com>
2026-04-29 20:07:57 +0300

    feat: nuxt config, package

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000'
+    }
+  },
+  devtools: { enabled: false }
 })
